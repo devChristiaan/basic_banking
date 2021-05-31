7459d65bd60446ad223ff0f56048643aa0327e14
Christiaan Hougaard <cwhougaard@gmail.com>
2021-05-30 20:34:41 -0400

    changes in css not commited

--- src/app.js
+++ src/app.js
@@ -5,7 +5,20 @@ const express = require('express')
 
 const app = express()
 
-
+//Setup basic func of app
 app.set('views', path.join(__dirname, './views'))
 
 app.set('view engine', 'ejs')
+
+app.use(express.static('./public'))
+
+
+///Main Routes
+app.get('/', (req,res) =>{
+  res.render('index', {title: 'Index'})
+})
+
+//Server
+app.listen(3000, () =>{
+  console.log("Server is running on localhost:3000");
+})
