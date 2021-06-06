25bdb995f6b2d6acd586c2d4e9ff01e330189b79
Christiaan Hougaard <cwhougaard@gmail.com>
2021-06-05 22:36:41 -0400

    fixed writing to file error. Changed accounts json in writeJSON function to variable not string.

--- src/data.js
+++ src/data.js
@@ -14,7 +14,7 @@ const users = JSON.parse(userData)
 //Write account Data
 const writeJSON = () => {
   let accountsJSON = JSON.stringify(accounts, null, 4)
-  fs.writeFileSync(path.join(__dirname, 'json', 'accounts.json'), 'accountsJSON' ,'utf8')
+  fs.writeFileSync(path.join(__dirname, 'json', 'accounts.json'), accountsJSON ,'utf8')
 }
 
 module.exports = {
